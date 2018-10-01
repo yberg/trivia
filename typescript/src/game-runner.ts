c582d953f9720f05347e1ca2f0844fb2606380e9
Viktor Yberg <viktor.yberg@slagkryssaren.com>
2018-10-01 14:56:30 +0200

    Print winner

--- typescript/src/game-runner.ts
+++ typescript/src/game-runner.ts
@@ -4,13 +4,16 @@ import {Player} from './player';
 export class GameRunner {
     public static main(): void {
         const game = new Game();
-        game.addPlayer(new Player("Chet"));
-        game.addPlayer(new Player("Pat"));
-        game.addPlayer(new Player("Sue"));
+        game.addPlayer(new Player('Chet'));
+        game.addPlayer(new Player('Pat'));
+        game.addPlayer(new Player('Sue'));
 
         while (game.next()) {
             console.log('-----------------------');
         }
+
+        console.log('-----------------------');
+        console.log(game.getCurrentPlayer().getName(), 'wins');
     }
 }
 
